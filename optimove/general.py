6a335378f8720f8ea8a04c0f6c9bc396ccfaf762
Nicolas RAMY <nicolas.ramy@darkelda.com>
2016-08-23 12:56:32 +0200

    add missing endpoints for general submodule

--- optimove/general.py
+++ optimove/general.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 
 from datetime import datetime, timedelta
+from urllib import quote_plus
 
 from . import URLBuilder
 
@@ -11,6 +12,11 @@ class General(URLBuilder):
     client = None
     username = None
     password = None
+
+    EVENT_TYPE_CAMPAIGN_PROCESSED = 1
+    EVENT_TYPE_TODAY_CAMPAIGNS_HAS_BEEN_PROCESSED = 2
+    EVENT_TYPE_DAILY_DATA_UPDATE_COMPLETED = 3
+    EVENT_TYPE_CAMPAIGN_CANCELED = 4
 
     def __init__(self, client):
         self.client = client
@@ -37,3 +43,30 @@ class General(URLBuilder):
         """Returns the date of the most recently available customer data"""
         response = self.client.get(self._get_url())
         return response.json()['Date'] if response else False
+
+    def register_event_listener(self, event_type_id, listener_url):
+        """Specifies the URL of a listener to which Optimove will report events of the specified type (e.g., “campaign
+        scheduled”)."""
+        if not event_type_id or not listener_url:
+            raise Exception('No EventTypeID and ListenerURL provided')
+
+        data = {
+            'EventTypeID': event_type_id,
+            'ListenerURL': quote_plus(listener_url)
+        }
+
+        response = self.client.post(self._get_url(), data)
+        return bool(response)
+
+    def unregister_event_listener(self, event_type_id):
+        """Instructs Optimove to stop reporting events of the specified event type
+        to a previously-registered listener."""
+        if not event_type_id:
+            raise Exception('No EventTypeID provided')
+
+        data = {
+            'EventTypeID': event_type_id
+        }
+
+        response = self.client.post(self._get_url(), data)
+        return bool(response)
