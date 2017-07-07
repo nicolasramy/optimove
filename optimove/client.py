d18641abc3d41a4d1a864a690e066a529b433498
Yandaar SOMDA <ysomda@deezer.com>
2017-07-07 15:36:59 +0200

    Set timeout for request to Optimove API

--- optimove/client.py
+++ optimove/client.py
@@ -24,7 +24,7 @@ class Client:
     token = None
     expire = None
 
-    def __init__(self, username=None, password=None):
+    def __init__(self, username=None, password=None, timeout=30):
         self.general = General(self)
         self.model = Model(self)
         self.actions = Actions(self)
@@ -32,6 +32,7 @@ class Client:
         self.customers = Customers(self)
         self.segments = Segments(self)
         self.integrations = Integrations(self)
+        self.timeout = timeout
 
         if username and password:
             self.general.login(username, password)
@@ -58,7 +59,13 @@ class Client:
 
         headers = headers if headers else self._headers()
         LOGGER.debug("GET request: url=%s, payload=%s, headers=%s", url, payload, headers)
-        response = requests.get(url, params=payload, headers=headers)
+
+        try:
+            response = requests.get(url, params=payload, headers=headers, timeout=self.timeout)
+        except requests.exceptions.Timeout as error:
+            LOGGER.error("Timeout reached, error=%s", error)
+            raise Exception('Timeout reached {}sec, url is {}'.format(self.timeout, url))
+
         LOGGER.debug("GET response: url=%s, response_data=%s", url, response.text)
         return self.dispatch_response(response)
 
@@ -69,7 +76,13 @@ class Client:
         headers = headers if headers else self._headers()
         data = json.dumps(payload)
         LOGGER.debug("POST request: url=%s, data=%s, headers=%s", url, payload, headers)
-        response = requests.post(url, data=data, headers=headers)
+
+        try:
+            response = requests.post(url, data=data, headers=headers, timeout=self.timeout)
+        except requests.exceptions.Timeout as error:
+            LOGGER.error("Timeout reached, error=%s", error)
+            raise Exception('Timeout reached {}sec, url is {}'.format(self.timeout, url))
+
         LOGGER.debug("POST response: url=%s, response_data=%s", url, response.text)
         return self.dispatch_response(response)
 
