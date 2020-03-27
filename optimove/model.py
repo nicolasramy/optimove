5b095a027233d24d4a53bb970b498e0c1e3352e8
Nicolas Ramy <nicolas.ramy@darkelda.com>
2020-03-27 12:09:38 +0100

    Porting to Python 3

--- optimove/model.py
+++ optimove/model.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from __future__ import absolute_import, unicode_literals
 
-from constants import AUTHORIZED_DELIMITERS, UNAUTHORIZED_DELIMITERS
+from optimove.constants import AUTHORIZED_DELIMITERS, UNAUTHORIZED_DELIMITERS
 
 
 class Model(object):
